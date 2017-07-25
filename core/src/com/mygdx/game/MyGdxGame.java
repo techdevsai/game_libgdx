9a24ad70971e7fd554f675d9bc8683d1c2e18d65
SAI KIRAN <saikiran.sunkari482@gmail.com>
2017-07-24 23:47:07 -0500

    First Success animation - Ball rotation

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -24,7 +24,8 @@ public class MyGdxGame extends ApplicationAdapter {
 	public void create () {
 		batch = new SpriteBatch();
 		ballrotation = new TextureAtlas(Gdx.files.internal("BallRotations.atlas"));
-        animate_balls = new Animation(1/5f, ballrotation.getRegions());
+        animate_balls = new Animation(1/10f, ballrotation.getRegions());
+
        // img = new Texture("BEEBEE.jpg");
 	}
     @Override
@@ -32,17 +33,29 @@ public class MyGdxGame extends ApplicationAdapter {
         batch.dispose();
        ballrotation.dispose();
     }
+
+    public void handleInput()
+    {
+        int touchX=Gdx.input.getX();
+        int touchY=Gdx.input.getY();
+    }
+
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0, 1, 0, 1);
+		Gdx.gl.glClearColor(0, 2, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
         time_passed += Gdx.graphics.getDeltaTime();
       //  batch.draw(img, 0, 0);
         //animate_balls.getKeyFrame(time_passed,true);
 
-        batch.draw((TextureRegion) animate_balls.getKeyFrame(time_passed, true), 100, 500);
-		batch.end();
+        batch.draw((TextureRegion) animate_balls.getKeyFrame(time_passed, true), 10, 50,Gdx.graphics.getWidth() / 10, Gdx.graphics.getHeight() /8);
+        //use below if code to get the touch coordinates
+      /*  if(Gdx.input.justTouched()){
+            System.out.println("X= "+Gdx.input.getX()+"Y= "+Gdx.input.getY());
+        }*/
+
+        batch.end();
 	}
 	
 
